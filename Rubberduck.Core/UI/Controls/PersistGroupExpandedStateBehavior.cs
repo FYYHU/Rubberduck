7499c8c8e3cefa6224ac8484ac1f2dedcccc73f2
Frank Hu <44072346+FYYHU@users.noreply.github.com>
2025-03-27 12:38:23 +0100

    magic

--- Rubberduck.Core/UI/Controls/PersistGroupExpandedStateBehavior.cs
+++ Rubberduck.Core/UI/Controls/PersistGroupExpandedStateBehavior.cs
@@ -4,29 +4,23 @@ using System.Windows.Controls;
 using System.Windows.Interactivity;
 using GongSolutions.Wpf.DragDrop.Utilities;
 
+
 namespace Rubberduck.UI.Controls
 {
-    // Courtesy of @aKzenT - this is a heavily refactored implementation of https://stackoverflow.com/a/15924044/4088852
     public class PersistGroupExpandedStateBehavior : Behavior<Expander>
     {
-        public static readonly DependencyProperty InitialExpandedStateProperty = DependencyProperty.Register(
-            "InitialExpandedState",
-            typeof(bool),
-            typeof(PersistGroupExpandedStateBehavior),
-            new PropertyMetadata(default(bool)));
+        public static readonly DependencyProperty InitialExpandedStateProperty =
+            DependencyProperty.Register(nameof(InitialExpandedState), typeof(bool),
+                typeof(PersistGroupExpandedStateBehavior), new PropertyMetadata(false));
 
-        public static readonly DependencyProperty GroupNameProperty = DependencyProperty.Register(
-            "GroupName",
-            typeof(object),
-            typeof(PersistGroupExpandedStateBehavior),
-            new PropertyMetadata(default(object)));
+        public static readonly DependencyProperty GroupNameProperty =
+            DependencyProperty.Register(nameof(GroupName), typeof(object),
+                typeof(PersistGroupExpandedStateBehavior), new PropertyMetadata(null));
 
         private static readonly DependencyProperty ExpandedStateStoreProperty =
-            DependencyProperty.RegisterAttached(
-                "ExpandedStateStore",
-                typeof(IDictionary<object, bool>),
-                typeof(PersistGroupExpandedStateBehavior),
-                new PropertyMetadata(default(IDictionary<object, bool>)));
+            DependencyProperty.RegisterAttached("ExpandedStateStore",
+                typeof(IDictionary<object, bool>), typeof(PersistGroupExpandedStateBehavior),
+                new PropertyMetadata(null));
 
         public bool InitialExpandedState
         {
@@ -44,20 +38,28 @@ namespace Rubberduck.UI.Controls
         {
             base.OnAttached();
 
-            var states = GetExpandedStateStore();
-            var expanded = !states.ContainsKey(GroupName ?? string.Empty) ? (bool?)null : states[GroupName ?? string.Empty];
+            // Ensure the visual tree is fully loaded before trying to access parents
+            AssociatedObject.Dispatcher.BeginInvoke(new System.Action(() =>
+            {
+                var states = GetExpandedStateStore();
+                if (states == null) return;
 
-            AssociatedObject.IsExpanded = expanded ?? InitialExpandedState;
+                var key = GroupName ?? AssociatedObject.GetHashCode();
+                if (!states.ContainsKey(key))
+                {
+                    states[key] = InitialExpandedState;
+                }
 
-            AssociatedObject.Expanded += OnExpanded;
-            AssociatedObject.Collapsed += OnCollapsed;
+                AssociatedObject.IsExpanded = states[key];
+                AssociatedObject.Expanded += OnExpanded;
+                AssociatedObject.Collapsed += OnCollapsed;
+            }));
         }
 
         protected override void OnDetaching()
         {
             AssociatedObject.Expanded -= OnExpanded;
             AssociatedObject.Collapsed -= OnCollapsed;
-
             base.OnDetaching();
         }
 
@@ -67,31 +69,26 @@ namespace Rubberduck.UI.Controls
 
         private void SetExpanded(bool expanded)
         {
-            var expandStateStore = GetExpandedStateStore();
-            //TODO: Remove this once GetExpandedStateStore is reliable.
-            if (expandStateStore != null)
+            var states = GetExpandedStateStore();
+            if (states != null)
             {
-                expandStateStore[GroupName ?? string.Empty] = expanded;
+                var key = GroupName ?? AssociatedObject.GetHashCode();
+                states[key] = expanded;
             }
         }
 
         private IDictionary<object, bool> GetExpandedStateStore()
         {
-            //FIXME: This is not reliable since the containing GroupItem does not necessarily have a VisualParent at the time of the request.
             if (!(AssociatedObject?.GetVisualAncestor<ItemsControl>() is ItemsControl items))
             {
                 return null;
             }
 
-            var states = (IDictionary<object, bool>)items.GetValue(ExpandedStateStoreProperty);
-
-            if (states != null)
+            if (!(items.GetValue(ExpandedStateStoreProperty) is IDictionary<object, bool> states))
             {
-                return states;
+                states = new Dictionary<object, bool>();
+                items.SetValue(ExpandedStateStoreProperty, states);
             }
-
-            states = new Dictionary<object, bool>();
-            items.SetValue(ExpandedStateStoreProperty, states);
 
             return states;
         }
